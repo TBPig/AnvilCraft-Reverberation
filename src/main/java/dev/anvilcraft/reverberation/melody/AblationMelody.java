1d7e6a7613e79c35d79824bd7fd31e9263df58dd
TBPig <147127248+TBPig@users.noreply.github.com>
2026-04-03 11:53:01 +0800

    回退anvillib和anvilcraft到1.5.x稳定版本

--- src/main/java/dev/anvilcraft/reverberation/melody/AblationMelody.java
+++ src/main/java/dev/anvilcraft/reverberation/melody/AblationMelody.java
@@ -2,36 +2,34 @@ package dev.anvilcraft.reverberation.melody;
 
 import dev.anvilcraft.reverberation.AnvilCraftReverberation;
 import dev.anvilcraft.reverberation.api.Melody;
-import dev.anvilcraft.reverberation.api.MergeSound;
 import dev.anvilcraft.reverberation.api.MergeSoundStore;
-import dev.anvilcraft.reverberation.api.SoundWave;
-import dev.dubhe.anvilcraft.init.block.ModBlocks;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
 
-import java.util.List;
-
+@SuppressWarnings("unused")
 public class AblationMelody extends Melody {
     @Override
     public boolean satisfy(MergeSoundStore mergeSoundStore) {
-        List<MergeSound> soundHistory = mergeSoundStore.getSoundHistory();
-        if (soundHistory.isEmpty()) return false;
-
-        MergeSound sound = soundHistory.getLast();
-        List<SoundWave> soundWaves = sound.getSoundWaves();
-        int frostEnergy = 0;
-        int emberEnergy = 0;
-        for (SoundWave soundWave : soundWaves) {
-            Block block = soundWave.timbre().block();
-             if (block.equals(ModBlocks.FROST_ANVIL.get())) {
-                 frostEnergy += soundWave.loudness();
-             } else if (block.equals(ModBlocks.EMBER_ANVIL.get())) {
-                 emberEnergy += soundWave.loudness();
-             }
-        }
-        return frostEnergy == emberEnergy && frostEnergy != 0;
+//        4. 消融：当前周期的合音中，带有余烬金属块/浮霜金属块音色的*砧音波*，提供的能量相等
+        // TODO: 1.6以后加入
+//        List<MergeSound> soundHistory = mergeSoundStore.getSoundHistory();
+//        if (soundHistory.isEmpty()) return false;
+//
+//        MergeSound sound = soundHistory.getLast();
+//        List<SoundWave> soundWaves = sound.getSoundWaves();
+//        int frostEnergy = 0;
+//        int emberEnergy = 0;
+//        for (SoundWave soundWave : soundWaves) {
+//            Block block = soundWave.timbre().block();
+//             if (block.equals(ModBlocks.FROST_ANVIL.get())) {
+//                 frostEnergy += soundWave.loudness();
+//             } else if (block.equals(ModBlocks.EMBER_ANVIL.get())) {
+//                 emberEnergy += soundWave.loudness();
+//             }
+//        }
+//        return frostEnergy == emberEnergy && frostEnergy != 0;
+        return false;
     }
-    
+
     @Override
     public ResourceLocation getId() {
         return AnvilCraftReverberation.of("ablation");
